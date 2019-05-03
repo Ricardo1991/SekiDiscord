2fb31625a6623fb2778907cac62761439d31f358
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2019-05-03 22:32:42 +0100

    Remove dead code on Useful.cs
    Old code from IRC Seki that got copy pasted

--- SekiDiscord/Useful.cs
+++ SekiDiscord/Useful.cs
@@ -38,59 +38,6 @@ namespace SekiDiscord
             return ul;
         }
 
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return Convert.ToBase64String(plainTextBytes);
-        }
-
-        public static string RemoveUserMode(string user)
-        {
-            char[] usermodes = { '@', '+', '%', '~', '&' };
-
-            if (usermodes.Any((s) => Convert.ToChar(user.Substring(0, 1)).Equals(s)))
-                return user.Substring(1).Trim();
-            else return user.Trim();
-        }
-
-        public static DateTime ConvertFromUnixTimestamp(double timestamp)
-        {
-            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return origin.AddSeconds(timestamp);
-        }
-
-        public static double ConvertToUnixTimestamp(DateTime date)
-        {
-            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            TimeSpan diff = date.ToUniversalTime() - origin;
-            return Math.Floor(diff.TotalSeconds);
-        }
-
-        public static char GetUserMode(string user)
-        {
-            if (string.IsNullOrWhiteSpace(user))
-            {
-                return '0';
-            }
-            switch (user[0])
-            {
-                case '@':
-                case '+':
-                case '%':
-                case '~':
-                case '&':
-                    return user[0];
-
-                default:
-                    return '0';
-            }
-        }
-
-        public static string GetTimestamp(DateTime value)
-        {
-            return value.ToString("mmssffff");
-        }
-
         public static string GetBetween(string strSource, string strStart, string strEnd)
         {
             int Start, End;
@@ -148,47 +95,6 @@ namespace SekiDiscord
             }
 
             return template;
-        }
-    }
-
-    public static class StringExt
-    {
-        public static string Truncate(this string value, int maxLength)
-        {
-            if (string.IsNullOrEmpty(value)) return value;
-            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
-        }
-    }
-
-    public static class GetCompilationDate
-    {
-        public static DateTime RetrieveLinkerTimestamp()
-        {
-            string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
-            const int c_PeHeaderOffset = 60;
-            const int c_LinkerTimestampOffset = 8;
-            byte[] b = new byte[2048];
-            System.IO.Stream s = null;
-
-            try
-            {
-                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-                s.Read(b, 0, 2048);
-            }
-            finally
-            {
-                if (s != null)
-                {
-                    s.Close();
-                }
-            }
-
-            int i = BitConverter.ToInt32(b, c_PeHeaderOffset);
-            int secondsSince1970 = BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
-            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
-            dt = dt.AddSeconds(secondsSince1970);
-            dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
-            return dt;
         }
     }
 }
