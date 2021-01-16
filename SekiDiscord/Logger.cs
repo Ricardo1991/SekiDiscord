c1530afe43ac5c54bb7e150f363c85815ec2de10
Lukas <lukasmilten@gmail.com>
2021-01-16 05:33:33 +0200

    Add Logger class

---
+++ SekiDiscord/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace SekiDiscord {
+    public class Logger<T> {
+        private Type ClassType { get; set; }
+
+        public Logger() {
+            ClassType = typeof(T);
+        }
+
+        public void Info(string message) {
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "[{0}][{1}]: {2}", GetDateTimeString(), ClassType.ToString(), message));
+        }
+
+        private string GetDateTimeString() {
+            return DateTime.Now.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+    }
+}
