f5634faaf54302499c432d5b2feb62e8037e2424
Lukas <lukasmilten@gmail.com>
2021-01-17 08:29:32 +0200

    Refactor application Main

--- SekiDiscord/Settings.Designer.cs
+++ SekiDiscord/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SekiDiscord {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -92,6 +92,30 @@ namespace SekiDiscord {
             }
             set {
                 this["limid"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("152322300954411008")]
+        public ulong ChibiID {
+            get {
+                return ((ulong)(this["ChibiID"]));
+            }
+            set {
+                this["ChibiID"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("!")]
+        public char commandChar {
+            get {
+                return ((char)(this["commandChar"]));
+            }
+            set {
+                this["commandChar"] = value;
             }
         }
     }
