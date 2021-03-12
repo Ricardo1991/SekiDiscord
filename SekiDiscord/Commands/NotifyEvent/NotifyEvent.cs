c789a98001b019949c7ec71ed5c863e1229ca03b
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2021-03-12 18:58:12 +0000

    Create base classes

---
+++ SekiDiscord/Commands/NotifyEvent/NotifyEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SekiDiscord.Commands.NotifyEvent
+{
+    internal class NotifyEvent
+    {
+        private string name;
+        private DateTime eventStart;
+        private TimeSpan repeatPeriod;
+
+        private bool enabled = false;
+        private HashSet<ulong> eventSubscribers = new();
+
+        public NotifyEvent(string name, DateTime eventStart, TimeSpan repeatPeriod)
+        {
+            this.eventStart = eventStart;
+            this.repeatPeriod = repeatPeriod;
+            this.name = name;
+        }
+
+        public bool Enabled { get => enabled; set => enabled = value; }
+
+        /// <summary>
+        /// Subscribe a user to the event
+        /// Returns true if the user was added, and false if it already existed.
+        /// </summary>
+        /// <param name="userID">Discord ID of user to subscribe</param>
+        /// <returns></returns>
+        public bool SubscribeUser(ulong userID)
+        {
+            return eventSubscribers.Add(userID);
+        }
+
+        /// <summary>
+        /// Unsubscribe a user from this event
+        /// Returns true if the user was found and removed, and false if it was not.
+        /// </summary>
+        /// <param name="userID">Discord ID of user to unsubscribe</param>
+        /// <returns></returns>
+        public bool UnsubscribeUser(ulong userID)
+        {
+            bool removeSuccessful = eventSubscribers.Remove(userID);
+
+            if (eventSubscribers.Count == 0)
+                Enabled = false;
+
+            return removeSuccessful;
+        }
+    }
+}
---
+++ SekiDiscord/Commands/NotifyEvent/NotifyEventManager.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SekiDiscord.Commands.NotifyEvent
+{
+    internal class NotifyEventManager
+    {
+        private const string NOTIFY_FILE_PATH = "TextFiles/notify.json";
+        public static List<NotifyEvent> NotifyEvents = new();
+        private static readonly Logger logger = new Logger(typeof(NotifyEventManager));
+
+        static NotifyEventManager()
+        {
+            NotifyEvents = ReadNotifyEvents();
+        }
+
+        private static List<NotifyEvent> ReadNotifyEvents()
+        {
+            List<NotifyEvent> notifyEvents = new();
+
+            if (File.Exists(NOTIFY_FILE_PATH))
+            {
+                try
+                {
+                    using StreamReader r = new StreamReader(NOTIFY_FILE_PATH);
+                    string json = r.ReadToEnd();
+                    notifyEvents = JsonConvert.DeserializeObject<List<NotifyEvent>>(json);
+                }
+                catch (JsonException e)
+                {
+                    logger.Error("COULD NOT READ NOTIFY EVENTS: " + e.Message);
+                }
+            }
+
+            return notifyEvents;
+        }
+
+        private static void SaveNotifyEvents(List<NotifyEvent> notifyEvents)
+        {
+            try
+            {
+                using StreamWriter w = File.CreateText(NOTIFY_FILE_PATH);
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(w, notifyEvents);
+            }
+            catch (JsonException e)
+            {
+                logger.Error("COULD NOT SAVE NOTIFY EVENTS: " + e.Message);
+            }
+        }
+    }
+}
